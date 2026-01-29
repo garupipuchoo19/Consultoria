d1141d82ca74661b980ed567cd81046ba0848720
Cristopher Isaac Leon Olvera <luxprower19@gmail.com>
2026-01-29 13:28:32 -0600

    Cambio en el login para su adaptacion a la BD

--- modulo-usuarios/login.php
+++ modulo-usuarios/login.php
@@ -11,29 +11,12 @@ if ($_POST) {
 
     if ($resultado->num_rows > 0) {
         $usuario = $resultado->fetch_assoc();
+
         if (password_verify($password, $usuario['password'])) {
             $_SESSION['usuario'] = $correo;
             header("Location: dashabord.php");
+            exit;
         }
     }
 }
 ?>
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <title>Login</title>
-    <link rel="stylesheet" href="../css/estilos.css">
-</head>
-<body>
-<div class="container">
-    <h2>Iniciar sesión</h2>
-    <form method="POST">
-        <input type="email" name="correo" placeholder="Correo" required>
-        <input type="password" name="password" placeholder="Contraseña" required>
-        <button>Entrar</button>
-    </form>
-</div>
-
-</body>
-</html>
