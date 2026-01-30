7c6992fc3b72e4ba8be0d62ad5dedc5e14286d53
JadeMontalvo <jadevalle81@gmail.com>
2026-01-29 18:23:24 -0600

    Creacion de respuues dentro de correos

---
+++ modulo-usuarios/enviar_correo.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+include("config/conexion.php");
+
+$correo = $_SESSION['usuario'];
+$mensaje = $_POST['mensaje'];
+
+$sql = "INSERT INTO solicitudes (correo_usuario, mensaje)
+        VALUES ('$correo','$mensaje')";
+$conexion->query($sql);
+
+$asunto = "Solicitud recibida – Consultoría Digital";
+$respuesta = "
+Estimado/a cliente:
+
+Hemos recibido su solicitud correctamente.
+Nuestro equipo se pondrá en contacto con usted a la brevedad.
+
+Agradecemos su confianza.
+
+Atentamente,
+Consultoría Digital Estratégica
+";
+
+mail($correo, $asunto, $respuesta);
+
+header("Location: dashboard.php");
